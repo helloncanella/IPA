b4cfc249bf6d08f242d2f3dc1dcc5d23af513179
Hellon Canella <helloncanella@gmail.com>
2016-11-28 01:09:38 -0200

    installing babel-plugin-module-alias

--- components/language-selector.js
+++ components/language-selector.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import { Container, Content, Thumbnail } from 'native-base'
 
+
+
 export class LanguageSelector extends Component {
+    
+    renderLanguageList(){
+        return 
+    }
+
     render() {
         return (
             <Container className="language-selector">
                 <Content>
-
+                    {this.renderLanguageList()}
                 </Content>    
             </Container>
         )
